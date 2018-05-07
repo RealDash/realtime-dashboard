c401b64cd6f0156cc54e8d854279393174777bf0
franko4don <franko4don@yahoo.com>
2018-05-08 00:15:36 +0100

    integrated gentella

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/generic/{wildcard}', function($wildcard){
+    return view('template.'.$wildcard);
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
